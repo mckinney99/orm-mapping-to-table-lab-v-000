45e6a41d296d9841cce7d56acf20adb110f78bb7
Eric McKinney <mckinney99@gmail.com>
2017-12-05 17:39:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,10 @@ class Student
     name TEXT,
     grade INTEGER
     )
-    
+    SQL
+    DB[:conn].execute(sql)
+  end
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
